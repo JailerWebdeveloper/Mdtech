1a772e069dee8f2db3dc68ef449b00f78ed6b69e
Jailerwebwork <jailerwebwork@gmail.com>
2024-02-11 20:13:27 -0500

    sitemap integrado por extension

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site:'https://mdtech.com.co',
+  integrations: [tailwind(), sitemap()]
 });
