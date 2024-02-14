8a5af04ad6e1954c13937b6ef9e090511f85f6b6
Jailerwebwork <jailerwebwork@gmail.com>
2024-02-14 15:51:19 -0500

    dist compresed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
+import compress from "astro-compress";
+
+import metaTags from "astro-meta-tags";
 
 // https://astro.build/config
 export default defineConfig({
-  site:'https://mdtech.com.co',
-  integrations: [tailwind(), sitemap()]
+  site: 'https://mdtech.com.co',
+  integrations: [tailwind(), sitemap(), compress(), metaTags()]
 });
