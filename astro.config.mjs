c3daa237303f46dd2d114a10e8b81f04dbf90edb
Jailerwebwork <jailerwebwork@gmail.com>
2024-02-19 23:38:04 -0500

    toda la traduccion y correciones visuales

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import compress from "astro-compress";
+import astroI18next from "astro-i18next";
 
 import metaTags from "astro-meta-tags";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mdtech.com.co',
-  integrations: [tailwind(), sitemap(), compress(), metaTags()]
+  integrations: [tailwind(), sitemap(), compress(), metaTags(),astroI18next()]
 });
